98c7d100f3fe285cbce7a790c2ab0f8c9dbb75d0
madisonelissa <madisonefreese@gmail.com>
2023-05-23 21:10:28 -0700

    deaf grandma solution

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,20 @@
 def deaf_grandma():
-
-
+    goodbye_counter = 0
+    while True:
+        question = input("Don't be shy, ask grandma a question: ")
+        if question == "GOODBYE":
+            if goodbye_counter == 1:
+                print ("LATER, SKATER!")
+                quit()
+            else: 
+                goodbye_counter += 1
+                print("LEAVING SO SOON?")
+        elif question == "":
+            print("WHAT?!")
+        elif question == question.upper():
+            print("NO, NOT SINCE 1946!")
+        else:
+            print("SPEAK UP, KID!")
+        continue
 
 deaf_grandma()
